dbf13a3ded653eb300790cf3d2de04d715561171
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-08 10:01:10 -0600

    turned child nodes into array and ran for each loop of them to receve the id of each block, as well as the text content

--- kiss.js
+++ kiss.js
@@ -1,17 +1,31 @@
 $(function (){
+  const rootElement = $('.container-fluid') 
+  console.log(rootElement.children().attr('id'))
+var timeBlockArray = Array.from(rootElement.children())
+// console.log(timeBlockArray.length) 
+
+
+timeBlockArray.forEach(element => {
+  let elementKey = element.id
+  let elementText = $(element).children().eq(1).text()
+  console.log(elementKey)
+  console.log(elementText)
+})
 
 
 $('.time-block').on('click', ".saveBtn", function (e) {
   let blockId = $(this).parent().attr('id')
   let blockText = $(this).prev().val()
   localStorage.setItem(blockId, (blockText))
-console.log(blockId)
-console.log(blockText)
+  console.log(`id: ${blockId}
+  text: ${blockText}`)
 })
 
 
 
-
+function targetBlock(params) {
+  
+}
 
 
 
