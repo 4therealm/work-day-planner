274c05c3916808be7f6fc381218663c7ef982aab
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-09 15:28:23 -0600

    cleaning up the js

--- kiss.js
+++ kiss.js
@@ -4,30 +4,26 @@ const timeBlockElements = Array.from(rootElement.children())
 
 let today = dayjs()
 let hour = parseInt(dayjs().hour())
-
-
 $('#currentDay').text(today)
 
 timeBlockElements.forEach(element => {
   const elementKey = element.id
-  const elementText = $(element).children().eq(1)// check here
+  const elementText = $(element).children().eq(1)
   const elementTime = parseInt(elementKey.replace( /^\D+/g, ''));
-  console.log(`the time of this block is ${elementTime}and the current hour is ${hour}, is this block the current block? ${elementTime === hour}`)
   const savedData = localStorage.getItem ( elementKey ); 
+  
   elementText.text(savedData)
-if (elementTime < hour) {
-  $(element).addClass('past')
-  // .removeClass('future', 'present')
-  //  console.log('past')
-   }else if(elementTime > hour){
-    $(element).addClass('future')
-    // .removeClass('present', "past")
-    // console.log('future')
-   }else{
-    $(element).addClass('present')
-    // .removeClass('future', 'past')
-    // console.log('present')
-   }})
+  
+  
+  if (elementTime < hour) {$(element).addClass('past')}
+  else if(elementTime > hour){$(element).addClass('future')}
+  else{$(element).addClass('present')}
+ 
+  console.log
+    (`the time of this block is ${elementTime}
+    and the current hour is ${hour}, 
+    is this block the current block? ${elementTime === hour}`)
+})
 
 
 $('.time-block').on('click', ".saveBtn", function (e) {
--- test.js
+++ test.js
@@ -3,30 +3,24 @@ $(function(){
 $('.time-block').each(function(){
     // console.log(`div${index}: ${this.id}`)
   // console.log($(this).children(".description").text()) 
-  const tb_key = this.id
-  const tb_textarea = $(this).children(".description")
-  const tb_time = parseInt(elementKey.replace( /^\D+/g, ''));
-  const savedData = localStorage.getItem ( tb_key )  || [];
+  const elementKey = this.id
+  const elementText = $(this).children(".description")
+  const elementTime = parseInt(elementKey.replace( /^\D+/g, ''));
+  const savedData = localStorage.getItem ( elementKey );
   tb_textarea.text(savedData)
-  console.log(`the time of this block is ${typeof tb_time}and the current hour is ${hour}`)
-  console.log(typeof hour)
+  console.log(`the time of this block is ${elementTime}and the current hour is ${hour}. this is the active time block for this hour ${elementTime===hour}`)
 })
 
 
 
 function evaluateTime() {
+  $('#currentDay').text(today)
     if (elementTime < hour) {
   $(this).addClass('past')
-  .removeClass('future', 'present')
-  //  console.log('past')
    }else if(elementTime > hour){
     $(this).addClass('future')
-    .removeClass('present', 'past')
-    // console.log('future')
    }else{
     $(this).addClass('present')
-    .removeClass('future', 'past')
-    // console.log('present')
    }
 }
 
